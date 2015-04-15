476f904ede25ac8152e0d5981e7a8de428325dd1
Alexander Demko <AlexanderDemko@users.noreply.github.com>
2015-04-15 16:31:47 +0000

    Added BibleNote.UI

---
+++ dev/trunk/BibleNote.UI.Data/UIContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BibleNote.UI.Data
+{
+    public class UIContext : DbContext
+    {
+        public UIContext()
+            : base("BibleNote.UI")
+        {
+
+        }
+    }
+}
