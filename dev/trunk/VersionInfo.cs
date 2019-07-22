4148e816dcb49d7652fa6418fc68737e71a51467
Alexander Demko <Alexander.Demko@live.com>
2019-07-22 10:13:21 +0300

    deleted BuildProcessTemplates folder

--- dev/trunk/VersionInfo.cs
+++ dev/trunk/VersionInfo.cs
@@ -1,5 +1,5 @@
 using System.Reflection;
 
-[assembly: AssemblyCopyright("Copyright © Sasha & Natasha 2011-2016")]
+[assembly: AssemblyCopyright("Copyright © Sasha & Natasha 2011-2019")]
 [assembly: AssemblyVersion("4.0.0.0")]
 [assembly: AssemblyFileVersion("4.0.0.0")]
