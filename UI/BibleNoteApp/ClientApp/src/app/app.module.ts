0281fba784ee1866b9690ad9d2f28c24f16a75f3
Alexander Demko <Alexander.Demko@gmail.com>
2020-10-23 15:32:09 +0300

    Added ngx-bootstrap

--- UI/BibleNoteApp/ClientApp/src/app/app.module.ts
+++ UI/BibleNoteApp/ClientApp/src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -15,7 +17,9 @@ import { NavigationProvidersListComponent } from './navigation-providers/list/na
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    BrowserAnimationsModule,
+    BsDropdownModule.forRoot()
   ],
   providers: [
     AppInitializer
